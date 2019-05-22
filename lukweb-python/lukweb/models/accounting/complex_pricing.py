1b81f97e47627de57851c124c275450ab4e59bd2
Matthias Valvekens <matthias.valvekens@gmail.com>
2019-05-22 02:06:53 +0200

    Migration updates

--- lukweb-python/lukweb/models/accounting/complex_pricing.py
+++ lukweb-python/lukweb/models/accounting/complex_pricing.py
@@ -6,8 +6,8 @@ from typing import Optional, Iterable
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
-from djmoney.forms import MoneyField
-from moneyed import Money, Decimal
+from djmoney.models.fields import MoneyField
+from moneyed import Money
 from django.utils.translation import (
     ugettext_lazy as _, pgettext_lazy
 )
