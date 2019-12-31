451c9f98b121ec44f8d772b8ad9dc824800b3fa7
Matthias Valvekens <matthias@mvalvekens.be>
2019-12-31 16:58:05 +0900

    Add more complicated models

--- tests/models.py
+++ tests/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models import OuterRef, Sum, ExpressionWrapper, F, Subquery
+from django.db.models.functions import Coalesce
+from djmoney.models.fields import MoneyField
+
 from double_entry import models as base
 
 
@@ -30,5 +34,88 @@ class SimpleCustomerPaymentSplit(base.BaseTransactionSplit):
     )
 
 
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    start = models.DateTimeField()
 
-# TODO: make more complicated models
+class TicketCustomer(base.TransactionPartyMixin):
+    name = models.CharField(max_length=100)
+
+
+class ReservationDebtQuerySet(base.BaseDebtQuerySet):
+    TOTAL_PRICE_FIELD = 'total_price_fromdb'
+    FACE_VALUE_FIELD = 'face_value_fromdb'
+
+    def with_total_price(self):
+        cls = self.__class__
+        actual_reservation_query = Ticket.objects.filter(
+            reservation__debt_id=OuterRef('pk')
+        ).order_by().values('reservation_id').annotate(
+            _total_ticket_price=Sum(
+                ExpressionWrapper(
+                    F('category__price') * F('count'),
+                    output_field=models.DecimalField()
+                )
+            )
+        ).values('_total_ticket_price')
+
+        return self.annotate(**{
+            cls.FACE_VALUE_FIELD: ExpressionWrapper(
+                Subquery(actual_reservation_query),
+                output_field=models.DecimalField()
+            ),
+            # static price takes precedence
+            cls.TOTAL_PRICE_FIELD: ExpressionWrapper(
+                Coalesce(F('static_price'), F(cls.FACE_VALUE_FIELD), 0),
+                output_field=models.DecimalField()
+            )
+        })
+
+
+class ReservationDebt(base.BaseDebtRecord):
+    owner = models.ForeignKey(
+        TicketCustomer, on_delete=models.CASCADE, related_name='reservations'
+    )
+
+    static_price = models.DecimalField(
+        decimal_places=4, max_digits=19, null=True, blank=True
+    )
+
+class Reservation(ReservationDebt):
+    debt = models.OneToOneField(
+        ReservationDebt, on_delete=models.CASCADE,
+        related_name='reservation', parent_link=True
+    )
+
+    # see if the magic is smart enough to recognise that this FK is NOT
+    # the one needed by the ledger management code
+    referred_by = models.ForeignKey(
+        SimpleCustomer, on_delete=models.CASCADE,
+        related_name='referrer'
+    )
+
+
+class TicketCategory(models.Model):
+    price = MoneyField(decimal_places=2, max_digits=6)
+
+class Ticket(models.Model):
+    category = models.ForeignKey(
+        TicketCategory, models.CASCADE, related_name='tickets'
+    )
+    count = models.PositiveSmallIntegerField()
+
+
+class ReservationPayment(base.BasePaymentRecord, base.ConcreteAmountMixin):
+    customer = models.ForeignKey(
+        TicketCustomer, on_delete=models.CASCADE, related_name='payments'
+    )
+
+class ReservationPaymentSplit(base.BaseDebtPaymentSplit):
+
+    reservation = models.ForeignKey(
+        ReservationDebt, related_name='splits', on_delete=models.CASCADE
+    )
+
+    payment = models.ForeignKey(
+        ReservationPayment, related_name='splits', on_delete=models.CASCADE
+    )
--- tests/test_magic.py
+++ tests/test_magic.py
@@ -59,3 +59,66 @@ class TestSimpleMagic(TestCase):
         self.assertEqual(
             models.SimpleCustomer.get_payment_remote_fk_column(), 'creditor_id'
         )
+
+
+class TestComplexMagic(TestCase):
+
+    def test_double_ledger_link(self):
+        self.assertEqual(
+            models.ReservationDebt.get_other_half_model(),
+            models.ReservationPayment
+        )
+        self.assertEqual(
+            models.ReservationPayment.get_other_half_model(),
+            models.ReservationDebt
+        )
+        self.assertEqual(
+            models.Reservation.get_other_half_model(),
+            models.ReservationPayment
+        )
+
+    def test_ledger_split_link(self):
+        self.assertEqual(
+            models.ReservationDebt.get_split_model(),
+            (models.ReservationPaymentSplit, 'reservation')
+        )
+        self.assertEqual(
+            models.ReservationPayment.get_split_model(),
+            (models.ReservationPaymentSplit, 'payment')
+        )
+
+    def test_split_manager_link(self):
+        # TODO: need instances to test public API
+        self.assertEqual(
+            models.ReservationDebt.split_manager_name, 'splits'
+        )
+
+        self.assertEqual(
+            models.ReservationPayment.split_manager_name, 'splits'
+        )
+
+    def test_customer_ledger_links(self):
+        self.assertEqual(
+            models.TicketCustomer.get_debt_model(),
+            models.ReservationDebt
+        )
+        self.assertEqual(
+            models.TicketCustomer.get_payment_model(),
+            models.ReservationPayment
+        )
+        self.assertEqual(
+            models.TicketCustomer.get_split_model(),
+            models.ReservationPaymentSplit
+        )
+        self.assertEqual(
+            models.TicketCustomer.get_debt_remote_fk(), 'owner'
+        )
+        self.assertEqual(
+            models.TicketCustomer.get_payment_remote_fk(), 'customer'
+        )
+        self.assertEqual(
+            models.TicketCustomer.get_debt_remote_fk_column(), 'owner_id'
+        )
+        self.assertEqual(
+            models.TicketCustomer.get_payment_remote_fk_column(), 'customer_id'
+        )
