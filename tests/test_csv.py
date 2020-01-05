3346a19057aac93f36a42f119c8f09ce7a455bba
Matthias Valvekens <matthias@mvalvekens.be>
2020-01-05 09:24:51 +0900

    Simple refund generation tests

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -15,6 +15,7 @@ from double_entry.forms.bulk_utils import (
 )
 from double_entry.forms.csv import BankTransactionInfo
 from double_entry.forms.utils import ErrorMixin
+from double_entry.models import GnuCashCategory
 from . import models
 from double_entry.forms import csv as forms_csv
 
@@ -62,6 +63,12 @@ SIMPLE_LOOKUP_TEST_RESULT_DATA = {
     'timestamp': PARSE_TEST_DATETIME,
 }
 
+SIMPLE_OVERPAID_CHECK = {
+    'transaction_party_id': 1, 'amount': Money(40, 'EUR'),
+    'timestamp': PARSE_TEST_DATETIME,
+}
+
+
 class TestErrorMixin(ErrorMixin):
 
     def __init__(self, test_case: TestCase, expected_error_lines: Set[int]):
@@ -83,6 +90,7 @@ class TestErrorMixin(ErrorMixin):
         self.lines_with_errors.update(line_nos)
 
 
+# noinspection DuplicatedCode
 class TestBankCSVs(TestCase):
     fixtures = ['reservations.json', 'simple.json']
 
@@ -197,3 +205,69 @@ class TestBankCSVs(TestCase):
         debt: models.SimpleCustomerDebt = models.SimpleCustomerDebt.objects\
             .with_payments().get(debtor_id=1)
         self.assertTrue(debt.paid)
+
+    def test_review_simple_resolved_transaction_paid_too_much(self):
+        error_context = ResolvedTransactionMessageContext()
+        resolved_transaction = ResolvedTransaction(
+            **SIMPLE_OVERPAID_CHECK, pipeline_section_id=-1,
+            message_context=error_context, do_not_skip=False
+        )
+        cust = models.SimpleCustomer.objects.get(pk=1)
+        prep = models.SimpleTransferPreparator(
+            resolved_transactions=[(cust, resolved_transaction)]
+        )
+        prep.review()
+        self.assertEqual(len(error_context.transaction_warnings), 1)
+        self.assertTrue('only 32.00' in error_context.transaction_warnings[0])
+        self.assertEqual(len(error_context.transaction_errors), 0)
+        self.assertEqual(len(prep.valid_transactions), 1)
+        pt, = prep.valid_transactions
+        le: models.SimpleCustomerPayment = pt.ledger_entry
+        self.assertEqual(le.total_amount, resolved_transaction.amount)
+        self.assertEqual(le.credit_remaining, Money(8, 'EUR'))
+
+    def test_commit_simple_resolved_transaction_paid_too_much_norefund(self):
+        error_context = ResolvedTransactionMessageContext()
+        resolved_transaction = ResolvedTransaction(
+            **SIMPLE_OVERPAID_CHECK, pipeline_section_id=-1,
+            message_context=error_context, do_not_skip=False
+        )
+        cust = models.SimpleCustomer.objects.get(pk=1)
+        prep = models.SimpleTransferPreparator(
+            resolved_transactions=[(cust, resolved_transaction)]
+        )
+        prep.commit()
+        debt: models.SimpleCustomerDebt = models.SimpleCustomerDebt.objects \
+            .with_payments().get(debtor_id=1, is_refund=False)
+        self.assertTrue(debt.paid)
+        refund_exists = models.SimpleCustomerDebt.objects \
+            .filter(debtor_id=1, is_refund=True).exists()
+        self.assertFalse(refund_exists)
+        pmt: models.SimpleCustomerPayment = models.SimpleCustomerPayment.objects \
+            .with_debts().get(creditor_id=1)
+        self.assertFalse(pmt.fully_matched)
+        self.assertEqual(pmt.credit_remaining, Money(8, 'EUR'))
+
+    def test_commit_simple_resolved_transaction_paid_too_much_refund(self):
+        error_context = ResolvedTransactionMessageContext()
+        resolved_transaction = ResolvedTransaction(
+            **SIMPLE_OVERPAID_CHECK, pipeline_section_id=-1,
+            message_context=error_context, do_not_skip=False
+        )
+        cust = models.SimpleCustomer.objects.get(pk=1)
+        prep = models.SimpleTransferPreparator(
+            resolved_transactions=[(cust, resolved_transaction)]
+        )
+        refund_cat = GnuCashCategory.get_category('refund')
+        prep.refund_credit_gnucash_account = refund_cat
+        prep.commit()
+        debt: models.SimpleCustomerDebt = models.SimpleCustomerDebt.objects \
+            .with_payments().get(debtor_id=1, is_refund=False)
+        self.assertTrue(debt.paid)
+        refund: models.SimpleCustomerDebt = models.SimpleCustomerDebt.objects \
+            .with_payments().get(debtor_id=1, is_refund=True)
+        self.assertTrue(refund.gnucash_category == refund_cat)
+        self.assertTrue(refund.fully_matched)
+        pmt: models.SimpleCustomerPayment = models.SimpleCustomerPayment.objects \
+            .with_debts().get(creditor_id=1)
+        self.assertTrue(pmt.fully_matched)
